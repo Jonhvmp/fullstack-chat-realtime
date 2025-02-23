ecbdfe7b5b52a1c812d7b00b38ade0b5cc84d4ae
Jonh Alex <122692601+Jonhvmp@users.noreply.github.com>
2025-02-23 00:41:16 -0300

    atualiza página de login para usar novo método de autenticação com GitHub e adiciona link para registro

--- client/src/app/(auth)/login/page.tsx
+++ client/src/app/(auth)/login/page.tsx
@@ -8,9 +8,10 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { GithubIcon } from 'lucide-react'
+import Link from 'next/link'
 
 export default function LoginPage() {
-  const { login, loginWithGithub } = useAuth()
+  const { login, SignInOrSignUpWithGithub } = useAuth()
   const router = useRouter()
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
@@ -29,8 +30,10 @@ export default function LoginPage() {
 
   const handleGithubLogin = async () => {
     try {
-      await loginWithGithub()
-      router.push('/chat')
+      await SignInOrSignUpWithGithub()
+      setTimeout(() => {
+        router.push('/chat')
+      }, 500)
     } catch (err: Error | unknown) {
       const errorMessage = err instanceof Error ? err.message : 'Erro ao efetuar login'
       setError(errorMessage)
@@ -41,7 +44,7 @@ export default function LoginPage() {
     <div className="flex items-center justify-center min-h-screen bg-gray-50">
       <Card className="w-full max-w-md p-4">
         <CardHeader>
-          <CardTitle>Login</CardTitle>
+          <CardTitle className='text-center'>Login</CardTitle>
         </CardHeader>
         <CardContent>
           {error && <div className="mb-4 text-red-500">{error}</div>}
@@ -68,7 +71,7 @@ export default function LoginPage() {
                 className="mt-1"
               />
             </div>
-            <Button type="submit" className="w-full">
+            <Button type="submit" className="w-full bg-green-400 text-black hover:bg-green-500">
               Entrar
             </Button>
           </form>
@@ -76,6 +79,13 @@ export default function LoginPage() {
           <div className="mt-4">
             <Button onClick={handleGithubLogin} className="w-full bg-black text-white">
               Entrar com Github <GithubIcon className="w-6 h-6 inline-block" />
+            </Button>
+          </div>
+
+          <div className="mt-4 text-center">
+            <span>Não tem uma conta?</span>
+            <Button variant="link" asChild>
+              <Link href="/register" className='text-blue-700'>Registrar</Link>
             </Button>
           </div>
         </CardContent>
