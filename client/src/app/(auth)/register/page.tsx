d4e2fd36e20547e9a078a80d78231f0985218dd9
Jonh Alex <122692601+Jonhvmp@users.noreply.github.com>
2025-02-23 00:41:23 -0300

    adiciona opção de registro com GitHub e aprimora a página de registro

--- client/src/app/(auth)/register/page.tsx
+++ client/src/app/(auth)/register/page.tsx
@@ -7,8 +7,12 @@ import { Label } from '@/components/ui/label'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { registerUser } from '@/services/authService'
+import { useAuth } from '@src/contexts/auth/AuthContext'
+import { GithubIcon } from 'lucide-react'
+import Link from 'next/link'
 
 export default function RegisterPage() {
+  const { SignInOrSignUpWithGithub } = useAuth()
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
@@ -26,10 +30,25 @@ export default function RegisterPage() {
 
     try {
       await registerUser({ name, email, password })
-      router.push('/chat')
+
+      setTimeout(() => {
+        router.push('/chat')
+      }, 500)
     } catch (err: Error | unknown) {
       const error = err as { response?: { data?: { message?: string } } }
       setError(error.response?.data?.message || 'Erro ao registrar')
+    }
+  }
+
+  const registerGithub = async () => {
+    try {
+      await SignInOrSignUpWithGithub()
+      setTimeout(() => {
+        router.push('/chat')
+      }, 500)
+    } catch (err: Error | unknown) {
+      const errorMessage = err instanceof Error ? err.message : 'Erro ao registrar'
+      setError(errorMessage)
     }
   }
 
@@ -37,7 +56,7 @@ export default function RegisterPage() {
     <div className="flex items-center justify-center min-h-screen bg-gray-50">
       <Card className="w-full max-w-md p-4">
         <CardHeader>
-          <CardTitle>Registro</CardTitle>
+          <CardTitle className='text-center'>Registro</CardTitle>
         </CardHeader>
         <CardContent>
           {error && <div className="mb-4 text-red-500">{error}</div>}
@@ -86,10 +105,23 @@ export default function RegisterPage() {
                 className="mt-1"
               />
             </div>
-            <Button type="submit" className="w-full">
+            <Button type="submit" className="w-full bg-green-400 text-black hover:bg-green-500 ">
               Registrar
             </Button>
           </form>
+
+          <div className="mt-4">
+            <Button onClick={registerGithub} className="w-full">
+              Registrar com Github <GithubIcon className="w-6 h-6 inline-block" />
+            </Button>
+          </div>
+
+          <div className="mt-4 text-center">
+            <span>Não tem uma conta?</span>
+            <Button variant="link" asChild>
+              <Link href="/login" className='text-blue-700'>Entrar</Link>
+            </Button>
+          </div>
         </CardContent>
       </Card>
     </div>
