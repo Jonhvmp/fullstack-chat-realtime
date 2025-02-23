7acb7cde349641d97a4727249479a2d9bd2fde90
Jonh Alex <122692601+Jonhvmp@users.noreply.github.com>
2025-02-23 00:41:36 -0300

    adiciona barra de navegação e botões de autenticação na página inicial

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,31 @@
+'use client'
+
+import { useAuth } from '@src/contexts/auth/AuthContext';
+import Link from 'next/link';
+import React from 'react';
+
+function AuthButtons() {
+  const { isAuthenticated, isLoading } = useAuth();
+
+  if (isLoading) {
+    return <div>Carregando...</div>;
+  }
+
+  if (isAuthenticated) {
+    return <Link href="/chat" className="bg-green-500 hover:bg-green-700 text-center text-white font-bold py-2 px-4 rounded">Entrar no Chat</Link>;
+  }
+
+  return <Link href="/login" className="bg-blue-500 hover:bg-blue-700 text-center text-white font-bold py-2 px-4 rounded">Login</Link>;
+}
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1 className="text-4xl font-bold text-center">Bem vindo ao Chat RealTime</h1>
+
+      <div className="flex items-center justify-center">
+        <AuthButtons />
+      </div>
     </div>
   );
 }
